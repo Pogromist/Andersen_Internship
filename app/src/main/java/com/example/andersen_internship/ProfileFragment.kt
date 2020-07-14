72c7862fe6305bd144673a0b6918fb05658831db
Pogromist <a.g.klyukin@gmail.com>
2020-07-14 14:53:27 +0300

    Added Simple Notification

--- app/src/main/java/com/example/andersen_internship/ProfileFragment.kt
+++ app/src/main/java/com/example/andersen_internship/ProfileFragment.kt
@@ -1,13 +1,61 @@
 package com.example.andersen_internship
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+    private val channelId = "com.example.andersen_internship.channel1"
+    private lateinit var notificationManager: NotificationManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        notificationManager = requireActivity().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelId, "MyChannel", "Description of my channel")
+
+        btnShowNotification.setOnClickListener {
+            displayNotification()
+        }
+    }
+
+    private fun displayNotification() {
+        val notificationId = 1
+        val notification = NotificationCompat.Builder(requireContext(), channelId)
+            .setContentTitle("Notification")
+            .setContentText("Description of notification")
+            .setSmallIcon(R.drawable.ic_favorite_black)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+        notificationManager?.notify(notificationId, notification)
+    }
+
+    private fun createNotificationChannel (id: String, name: String, channelDescription: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = channelDescription
+            }
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
 }
+
+
