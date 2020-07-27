66411db809a7f7eb438e44e2e0f336f8def7f398
Pogromist <a.g.klyukin@gmail.com>
2020-07-27 15:50:13 +0300

    Fixed Retrofit request

--- app/src/main/java/com/example/andersen_internship/ProfileFragment.kt
+++ app/src/main/java/com/example/andersen_internship/ProfileFragment.kt
@@ -5,13 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_profile.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class ProfileFragment : Fragment() {
 
@@ -37,26 +33,8 @@ class ProfileFragment : Fragment() {
             createPost(requireContext())
         }
     }
-
-    /*fun createPost() {
-        NetworkService.instance.Api()
-            ?.getMovies(getString(R.string.api_key))
-            ?.enqueue(object : Callback<PopularMovies>{
-                override fun onFailure(call: Call<PopularMovies>, t: Throwable) {
-                    Toast.makeText(context, "onFailure", Toast.LENGTH_SHORT).show()
-                }
-
-                override fun onResponse(
-                    call: Call<PopularMovies>,
-                    response: Response<PopularMovies>
-                ) {
-                    if (response.isSuccessful) {
-                        Toast.makeText(context, "Success request", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            })
-    }*/
 }
+
 
 
 
