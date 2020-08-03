191d54a1b5f7ced6b718ea5d17b1f76b067073a0
Pogromist <a.g.klyukin@gmail.com>
2020-08-03 17:14:00 +0300

    Transfer logic to Presenter

--- app/src/main/java/com/example/andersen_internship/ProfileFragment.kt
+++ app/src/main/java/com/example/andersen_internship/ProfileFragment.kt
@@ -5,23 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.andersen_internship.mvp.ProfilePresenter
 import com.example.andersen_internship.mvp.ProfileView
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_profile.*
 import moxy.MvpAppCompatFragment
-import moxy.presenter.InjectPresenter
 
 class ProfileFragment : MvpAppCompatFragment(), ProfileView {
 
-    @InjectPresenter
-    lateinit var profilePresenter: ProfilePresenter
-
-    lateinit var myNotification: MyNotification
+    private var profilePresenter = ProfilePresenter()
     private val compositeDisposable = CompositeDisposable()
 
     override fun onCreateView(
@@ -35,16 +28,13 @@ class ProfileFragment : MvpAppCompatFragment(), ProfileView {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        myNotification = MyNotification.instance
-
         btnShowNotification.setOnClickListener {
-           profilePresenter.onNotificationClick()
+           profilePresenter.onNotificationClick(requireActivity())
         }
 
         btnRetrofitRequest.setOnClickListener {
-            profilePresenter.onRetrofitRequestClick()
+            profilePresenter.onRetrofitRequestClick(requireContext())
         }
-
     }
 
     override fun onDestroyView() {
@@ -52,27 +42,9 @@ class ProfileFragment : MvpAppCompatFragment(), ProfileView {
         compositeDisposable.dispose()
     }
 
-    private fun onFailure(t: Throwable) {
-        Toast.makeText(context, "onFailure", Toast.LENGTH_SHORT).show()
-    }
+    override fun retrofitRequest() {}
 
-    private fun onResponse(response: PopularMovies) {
-        Toast.makeText(context, "Success request", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun retrofitRequest() {
-        compositeDisposable.add(
-            NetworkService.buildService().getMovies(getString(R.string.api_key))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe({ response -> onResponse(response) }, { t -> onFailure(t) })
-        )
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun notification() {
-        myNotification.notification(requireActivity())
-    }
+    override fun notification() {}
 }
 
 
--- app/src/main/java/com/example/andersen_internship/mvp/ProfilePresenter.kt
+++ app/src/main/java/com/example/andersen_internship/mvp/ProfilePresenter.kt
@@ -1,14 +1,45 @@
 package com.example.andersen_internship.mvp
 
+import android.content.Context
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.FragmentActivity
+import com.example.andersen_internship.MyNotification
+import com.example.andersen_internship.NetworkService
+import com.example.andersen_internship.PopularMovies
+import com.example.andersen_internship.R
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import moxy.InjectViewState
 import moxy.MvpPresenter
 
 @InjectViewState
 class ProfilePresenter : MvpPresenter<ProfileView>() {
-    fun onRetrofitRequestClick() {
-        viewState.retrofitRequest()
+
+    private val compositeDisposable = CompositeDisposable()
+    var myNotification = MyNotification.instance
+
+    fun onRetrofitRequestClick(context: Context) {
+        compositeDisposable.add(
+            NetworkService.buildService().getMovies(context.getString(R.string.api_key))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ response -> onResponse(response, context) }, { t -> onFailure(t, context) })
+        )
     }
-    fun onNotificationClick() {
-        viewState.notification()
+
+    private fun onFailure(t: Throwable, context: Context) {
+        Toast.makeText(context, "onFailure", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onResponse(response: PopularMovies, context: Context) {
+        Toast.makeText(context, "Success request", Toast.LENGTH_SHORT).show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun onNotificationClick(activity: FragmentActivity) {
+        myNotification.notification(activity)
     }
 }
